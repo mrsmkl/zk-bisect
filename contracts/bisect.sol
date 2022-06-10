a1cc9a6b9ececa724faf46fd2353b038eef18dd0
Sami Mäkelä <mrsmkl@gmail.com>
2022-06-10 14:02:58 +0300

    first version of contract

--- contracts/bisect.sol
+++ contracts/bisect.sol
@@ -13,16 +13,23 @@ contract Bisect {
         chal = Verifierbisectchallenge(b);
     }
 
+    uint constant TURNS = 40;
+
     struct Challenge {
         uint state;
+        address other;
+        address owner;
         uint[2] key1;
         uint[2] key2;
+        uint turn;
+        uint[2] choose;
     }
 
     mapping (bytes32 => Challenge) challenges;
 
     function initChallenge(
         bytes32 id,
+        address other,
         uint[2] memory key1,
         uint[2] memory key2,
         uint[2] memory step1,
@@ -57,6 +64,59 @@ contract Bisect {
         challenges[id].key1 = key1;
         challenges[id].key2 = key2;
         challenges[id].state = state;
+        challenges[id].other = other;
+        challenges[id].owner = msg.sender;
+    }
+
+    function queryChallenge(
+        bytes32 id,
+        uint[2] memory choose
+    ) public {
+        require(challenges[id].other == msg.sender, "only other can query");
+        require(challenges[id].choose[0] == 0, "already queried");
+        challenges[id].choose = choose;
+    }
+
+    function replyChallenge(
+        bytes32 id,
+        uint[2] memory step1,
+        uint[2] memory hash1,
+        uint[2] memory step2,
+        uint[2] memory hash2,
+        uint[2] memory step3,
+        uint[2] memory hash3,
+        uint state,
+        bytes memory proof
+    ) public {
+        Challenge storage c = challenges[id];
+        require(c.owner == msg.sender, "only owner can query");
+        require(c.choose[0] != 0, "not queried");
+        uint[] memory params = new uint[](20);
+        params[0] = c.key1[0];
+        params[1] = c.key1[1];
+        params[2] = c.key2[0];
+        params[3] = c.key2[1];
+        params[4] = step1[0];
+        params[5] = step1[1];
+        params[6] = hash1[0];
+        params[7] = hash1[1];
+        params[8] = step2[0];
+        params[9] = step2[1];
+        params[10] = hash2[0];
+        params[11] = hash2[1];
+        params[12] = step3[0];
+        params[13] = step3[1];
+        params[14] = hash3[0];
+        params[15] = hash3[1];
+        params[16] = c.choose[0];
+        params[17] = c.choose[1];
+        params[18] = state;
+        params[19] = c.state;
+        chal.verifyProof(proof, params);
+        c.choose[0] = 0;
+        c.state = state;
+        c.turn++;
     }
 
 }
+
